299498d86529b188f7cc7d6f3904b4d7d6bb423b
jesualujan <lujan.oficial@gmail.com>
2023-09-14 18:25:16 -0600

    ✨ llamada a la api

--- 02_openweather/src/Components/WeatherApp/WeaterApp.jsx
+++ 02_openweather/src/Components/WeatherApp/WeaterApp.jsx
@@ -9,10 +9,8 @@ import wind_icon from '../Assets/wind.png'
 function WeaterApp() {
     //* AQUÍ VA LA LÓGICA DE NUESTRO COMPONENTE 
 
-    
-
-    const search = () =>{
-        const element = document.getElementsByClassName("cityInput")
+    const search = async () =>{
+        const element = document.getElementsByClassName("cityInput");
         if(element[0].value===''){
             return 0
         }
@@ -20,12 +18,21 @@ function WeaterApp() {
         let URI = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&unit&lat=44.34&lon=10.99&appid=${process.env.REACT_APP_API_KEY}`
 
         {/* HAGO LA LLAMADA A LA API*/}
+        let response = await fetch(URI)
+        let data = await response.json()
    
-        let response = fetch(URI)
-        let data = response.json()
-   
+        {/* SELECCIONAR ELEMENTO */}
+        const humidity = document.getElementsByClassName("humidity-percent")
+        const wind = document.getElementsByClassName("wind-velocity")
+        const temperature = document.getElementsByClassName ("weather-temp")
+        const location = document.getElementsByClassName("weather-location")
 
-    
+        {/* MANIPULAR ELEMENTO */}
+        humidity[0].innerHTML= data.main.humidity+"%"
+        wind[0].innerHTML=data.wind.speed+"km/h"
+        temperature[0].innerHTML=data.main.temp+"°F"
+        location[0].innerHTML=data.name
+
     }
 
    
@@ -36,7 +43,7 @@ function WeaterApp() {
         <div className="container">
             <div className="top-bar">
                 <input type="text" className="cityInput" placeholder="Buscar"/>
-                <div className="search-icon">
+                <div className="search-icon" onClick={()=>{search()}}>
                     <img src={search_icon} alt="search_icon" />
                 </div>
             </div>
